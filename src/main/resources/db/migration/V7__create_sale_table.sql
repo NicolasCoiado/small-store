0500536e4fe3b522923248705daf158e80c01259
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-11-23 10:18:56 -0300

    feat: adds the migration that creates the sale table

---
+++ src/main/resources/db/migration/V7__create_sale_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sale (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER,
+    company_id INTEGER NOT NULL,
+    sale_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    total_value DECIMAL(12,2) NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (customer_id) REFERENCES customer(id),
+    FOREIGN KEY (company_id) REFERENCES company(id)
+);
