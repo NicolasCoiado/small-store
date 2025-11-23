6ed2726f660f0e491c8ca040089b4ac4c3050060
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-11-23 10:17:57 -0300

    feat: adds the migration that creates the product table

---
+++ src/main/resources/db/migration/V6__create_product_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER NOT NULL DEFAULT 0,
+    company_id INTEGER NOT NULL,
+    FOREIGN KEY (company_id) REFERENCES company(id)
+);
