88ed414015167946c6ab811ba928d98f37eb316b
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-11-23 10:19:24 -0300

    feat: adds the migration that creates the sale_item table

---
+++ src/main/resources/db/migration/V8__create_sale_item_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sale_item (
+    id SERIAL PRIMARY KEY,
+    sale_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    unit_price DECIMAL(10,2) NOT NULL,
+    total_price DECIMAL(12,2) GENERATED ALWAYS AS (quantity * unit_price) STORED,
+
+    FOREIGN KEY (sale_id) REFERENCES sale(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
